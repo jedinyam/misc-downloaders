f91f23547e3a2b739d8ec581681a16398790d17e
= <=>
2019-08-19 08:37:59 -0400

    Removed unused files, derp

--- utils.py
+++
@@ -1,9 +0,0 @@
-import requests
-
-
-def download_image(url, image):
-    with open(image, "wb") as file:
-        response = requests.get(url)
-
-        if response.status_code is requests.codes.ok:
-            file.write(response.content)
